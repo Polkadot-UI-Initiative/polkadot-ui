6ac1d5bc6ff1a4bdaf7b621d4e418fa662465f71
Niklas P <contact@niklasplessing.net>
2025-07-23 17:05:20 +0200

    remove unavailable blocks

--- packages/registry/components/animated-code-block.tsx
+++ packages/registry/components/animated-code-block.tsx
@@ -7,6 +7,20 @@ import { Copy, Check } from "lucide-react";
 
 const packageManagers = [
   {
+    name: "npm",
+    value: "npm",
+    command: "npx dot-ui add",
+    icon: (
+      <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <title>npm</title>
+        <path
+          className="fill-current"
+          d="M1.763 0C.786 0 0 .786 0 1.763v20.474C0 23.214.786 24 1.763 24h20.474c.977 0 1.763-.786 1.763-1.763V1.763C24 .786 23.214 0 22.237 0zM5.13 5.323l13.837.019-.009 13.836h-3.464l.01-10.382h-3.456L12.04 19.17H5.113z"
+        />
+      </svg>
+    ),
+  },
+  {
     name: "pnpm",
     value: "pnpm",
     command: "pnpm dlx dot-ui add",
@@ -35,20 +49,6 @@ const packageManagers = [
     ),
   },
   {
-    name: "npm",
-    value: "npm",
-    command: "npx dot-ui add",
-    icon: (
-      <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-        <title>npm</title>
-        <path
-          className="fill-current"
-          d="M1.763 0C.786 0 0 .786 0 1.763v20.474C0 23.214.786 24 1.763 24h20.474c.977 0 1.763-.786 1.763-1.763V1.763C24 .786 23.214 0 22.237 0zM5.13 5.323l13.837.019-.009 13.836h-3.464l.01-10.382h-3.456L12.04 19.17H5.113z"
-        />
-      </svg>
-    ),
-  },
-  {
     name: "yarn",
     value: "yarn",
     command: "yarn dlx dot-ui add",
@@ -64,16 +64,10 @@ const packageManagers = [
   },
 ];
 
-const blocks = [
-  "block-number",
-  "example-form",
-  "example-with-css",
-  "hello-world",
-  "complex-component",
-];
+const blocks = ["address-input"];
 
 export function AnimatedCodeBlock() {
-  const [activeTab, setActiveTab] = useState("pnpm");
+  const [activeTab, setActiveTab] = useState("npm");
   const [currentBlock, setCurrentBlock] = useState(0);
   const [isAnimating, setIsAnimating] = useState(false);
   const [isCopied, setIsCopied] = useState(false);
@@ -109,7 +103,7 @@ export function AnimatedCodeBlock() {
   return (
     <div className="max-w-[480px] lg:w-[480px] mx-auto">
       <Tabs
-        defaultValue="pnpm"
+        defaultValue="npm"
         value={activeTab}
         onValueChange={setActiveTab}
         className="w-full"
