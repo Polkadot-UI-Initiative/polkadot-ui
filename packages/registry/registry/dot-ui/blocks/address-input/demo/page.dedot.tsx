2dab93964a0a616cac447596c7972846ceb0aca7
Niklas P <contact@niklasplessing.net>
2025-07-31 23:18:58 +0200

    fix missing registry items

--- packages/registry/registry/dot-ui/blocks/address-input/demo/page.dedot.tsx
+++ packages/registry/registry/dot-ui/blocks/address-input/demo/page.dedot.tsx
@@ -1,6 +1,6 @@
 import { PolkadotProvider } from "@/registry/dot-ui/providers/dedot-provider";
 import { AddressInput } from "@/registry/dot-ui/blocks/address-input/components/address-input.dedot";
-import AddressInputDemo from "@/registry/dot-ui/blocks/address-input/demo/address-input-demo";
+import { AddressInputDemo } from "@/registry/dot-ui/blocks/address-input/demo/address-input-demo";
 
 export default function AddressInputDedotPage() {
   return (
