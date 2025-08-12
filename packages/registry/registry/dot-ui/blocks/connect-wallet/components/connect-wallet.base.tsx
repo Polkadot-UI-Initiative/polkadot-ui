3ec7056ff2b9636794a6e9733c94b3070e03d4c0
ical10 <mochhusnir@gmail.com>
2025-08-12 12:55:54 +0800

    fix: add missing files to registry and fix import

--- packages/registry/registry/dot-ui/blocks/connect-wallet/components/connect-wallet.base.tsx
+++ packages/registry/registry/dot-ui/blocks/connect-wallet/components/connect-wallet.base.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Wallet, ExtensionWallet } from "typink";
-import { Button } from "@/registry/dot-ui/ui/button";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { ChevronDown } from "lucide-react";
 import {
