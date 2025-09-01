6cf06868ba50ff57b78554baa450061f0361b9da
Niklas P <contact@niklasplessing.net>
2025-09-01 11:54:17 +0200

    fix import

--- packages/registry/registry/dot-ui/blocks/connect-wallet/demo/page.dedot.tsx
+++ packages/registry/registry/dot-ui/blocks/connect-wallet/demo/page.dedot.tsx
@@ -1,6 +1,6 @@
 import { PolkadotProvider } from "@/registry/dot-ui/providers/dedot-provider";
 import { ConnectWalletDemo } from "@/registry/dot-ui/blocks/connect-wallet/demo/connect-wallet-demo";
-import { WalletSelect } from "../components/wallet-select.dedot";
+import { WalletSelect } from "@/registry/dot-ui/blocks/connect-wallet/components/wallet-select.dedot";
 
 export default function ConnectWalletDedotPage() {
   return (
