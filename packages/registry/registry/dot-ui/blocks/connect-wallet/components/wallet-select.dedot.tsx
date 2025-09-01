29acb8a525c31bc7b008843560d80a23e54cf940
Niklas P <contact@niklasplessing.net>
2025-09-01 12:01:00 +0200

    simplify demo

--- packages/registry/registry/dot-ui/blocks/connect-wallet/components/wallet-select.dedot.tsx
+++ packages/registry/registry/dot-ui/blocks/connect-wallet/components/wallet-select.dedot.tsx
@@ -1,13 +1,9 @@
 "use client";
-
-import { useMemo } from "react";
 import { ClientOnly } from "@/registry/dot-ui/blocks/client-only";
 import { PolkadotProvider } from "@/registry/dot-ui/providers/dedot-provider";
 import {
   WalletSelectBase,
   type WalletSelectBaseProps,
-  type AccountInfo,
-  type WalletInfo,
 } from "@/registry/dot-ui/blocks/connect-wallet/components/wallet-select.base";
 import { useTypink } from "typink";
 
@@ -24,32 +20,20 @@ export function WalletSelect(props: WalletSelectProps) {
     setConnectedAccount,
   } = useTypink();
 
-  const services = useMemo(
-    () => ({
-      accounts: accounts as unknown as AccountInfo[],
-      wallets: wallets as unknown as WalletInfo[],
-      connectedWallets: connectedWallets as unknown as WalletInfo[],
-      connectedAccount: connectedAccount as unknown as AccountInfo | undefined,
-      connectWallet,
-      disconnect,
-      setConnectedAccount: setConnectedAccount as unknown as (
-        account: AccountInfo
-      ) => void,
-    }),
-    [
-      accounts,
-      wallets,
-      connectedWallets,
-      connectedAccount,
-      connectWallet,
-      disconnect,
-      setConnectedAccount,
-    ]
-  );
-
   return (
     <ClientOnly>
-      <WalletSelectBase {...props} services={services} />
+      <WalletSelectBase
+        {...props}
+        services={{
+          accounts,
+          wallets,
+          connectedWallets,
+          connectedAccount,
+          connectWallet,
+          disconnect,
+          setConnectedAccount,
+        }}
+      />
     </ClientOnly>
   );
 }
