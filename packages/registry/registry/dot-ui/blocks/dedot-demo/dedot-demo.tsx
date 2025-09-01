292556f6bb9709abb228761a80258171677b57cd
Niklas P <contact@niklasplessing.net>
2025-09-01 02:04:48 +0200

    try provider

--- packages/registry/registry/dot-ui/blocks/dedot-demo/dedot-demo.tsx
+++ packages/registry/registry/dot-ui/blocks/dedot-demo/dedot-demo.tsx
@@ -1,5 +1,26 @@
+"use client";
+
 import { PolkadotProvider } from "@/registry/dot-ui/providers/dedot-provider";
+import { ClientConnectionStatus, useTypink } from "typink";
 
 export default function DedotDemo() {
-  return <PolkadotProvider>something with providers</PolkadotProvider>;
+  return (
+    <PolkadotProvider>
+      <List />
+    </PolkadotProvider>
+  );
+}
+
+export function List() {
+  const { connectionStatus, network, connectedWalletIds } = useTypink();
+  return (
+    <div>
+      network: {network.name}
+      isConnected:{" "}
+      {connectionStatus.get("paseo_people") === ClientConnectionStatus.Connected
+        ? "true"
+        : "false"}
+      connectedWalletIds: {connectedWalletIds?.join(", ")}
+    </div>
+  );
 }
