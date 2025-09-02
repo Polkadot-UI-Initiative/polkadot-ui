2e83afc263180bddd30fcc0d8ecd11f12b58a4c4
Niklas P <contact@niklasplessing.net>
2025-09-02 09:48:21 +0200

    txbutton

--- packages/registry/components/remark-button.tsx
+++ packages/registry/components/remark-button.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { TxButtonBase } from "@/registry/dot-ui/blocks/tx-button/components/tx-button.base";
+import { TxButton } from "@/registry/dot-ui/blocks/tx-button/components/tx-button.dedot";
 import { NetworkId, useTx, useTypink } from "typink";
 
 export function RemarkButton({ networkId }: { networkId: NetworkId }) {
@@ -11,7 +11,7 @@ export function RemarkButton({ networkId }: { networkId: NetworkId }) {
   });
 
   return (
-    <TxButtonBase
+    <TxButton
       tx={tx}
       args={["Hello World from polkadot-ui"]}
       networkId={networkId}
@@ -20,6 +20,6 @@ export function RemarkButton({ networkId }: { networkId: NetworkId }) {
       Remark a message on{" "}
       {/* eslint-disable-next-line @next/next/no-img-element */}
       <img src={network?.logo} alt={network?.name} className="w-4 h-4" />
-    </TxButtonBase>
+    </TxButton>
   );
 }
