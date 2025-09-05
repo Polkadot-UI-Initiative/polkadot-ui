c5691f90ba5ead568d4799d3e195a7230003a59f
Niklas P <contact@niklasplessing.net>
2025-09-05 18:43:52 +0200

    txbutton

--- packages/cli/src/commands/add.ts
+++ packages/cli/src/commands/add.ts
@@ -646,7 +646,7 @@ export class AddCommand {
 
     // If no chains are configured, install defaults
     logger.info("No chains configured, installing defaults");
-    const defaultChains = ["paseo", "paseo_people"];
+    const defaultChains = ["paseo", "paseoPeople"];
 
     logger.detail(`Installing default chains: ${defaultChains.join(", ")}`);
     await this.installMissingChains(defaultChains);
