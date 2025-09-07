a6cf5e1a491e6fdde7207af9f7cf539a47736815
Niklas P <niklasp@users.noreply.github.com>
2025-09-07 22:04:42 +0200

    Version0.9 (#88)
    * tx-button init
    
    * update
    
    * fix names in registries
    
    * txbutton
    
    * txbutton
    
    * wip
    
    * txbutton
    
    * txbutton
    
    * fix build with dedot
    
    * registry
    
    * registry + mcp
    
    * mcp + registry
    
    * mcp + registry
    
    * mcp + registry
    
    * mcp + registry
    
    * mcp + registry
    
    * mcp + registry
    
    * mcp + registry

---
+++ packages/registry/components/logo.tsx
@@ -0,0 +1,37 @@
+export function Logo({ props }: { props: React.SVGProps<SVGSVGElement> }) {
+  return (
+    <svg
+      width="424px"
+      height="424px"
+      viewBox="0 0 424 424"
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      {...props}
+    >
+      <title>Artboard</title>
+      <desc>Created with Sketch.</desc>
+      <g
+        id="Artboard"
+        stroke="none"
+        stroke-width="1"
+        fill="none"
+        fill-rule="evenodd"
+      >
+        <g id="polkadot-ui_logo_symbol_white">
+          <path
+            d="M64,0 L136,0 C171.346224,-6.49299601e-15 200,28.653776 200,64 L200,360 C200,395.346224 171.346224,424 136,424 L64,424 C28.653776,424 4.32866401e-15,395.346224 0,360 L0,64 C-4.32866401e-15,28.653776 28.653776,6.49299601e-15 64,0 Z M72,36 C52.117749,36 36,52.117749 36,72 L36,352 C36,371.882251 52.117749,388 72,388 L128,388 C147.882251,388 164,371.882251 164,352 L164,72 C164,52.117749 147.882251,36 128,36 L72,36 Z"
+            id="Combined-Shape"
+            fill="currentColor"
+          ></path>
+          <path
+            d="M288,224 L360,224 C395.346224,224 424,252.653776 424,288 L424,360 C424,395.346224 395.346224,424 360,424 L288,424 C252.653776,424 224,395.346224 224,360 L224,288 C224,252.653776 252.653776,224 288,224 Z M290,260 C273.431458,260 260,273.431458 260,290 L260,358 C260,374.568542 273.431458,388 290,388 L358,388 C374.568542,388 388,374.568542 388,358 L388,290 C388,273.431458 374.568542,260 358,260 L290,260 Z"
+            id="Combined-Shape"
+            fill="currentColor"
+          ></path>
+          <circle id="Oval" fill="#F9286F" cx="324" cy="100" r="100"></circle>
+        </g>
+      </g>
+    </svg>
+  );
+}
