3343ebf806643e332fec6bbad80fd4f3022c5457
ical10 <mochhusnir@gmail.com>
2025-09-26 16:58:57 +0800

    fix: incorrect impot

--- packages/registry/__tests__/amount-input.base.test.tsx
+++ packages/registry/__tests__/amount-input.base.test.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
-import { AmountInputBase } from "@/registry/polkadot-ui/blocks/amount-input/components/amount-input.base";
+import { AmountInputBase } from "@/registry/polkadot-ui/blocks/amount-input/amount-input.base";
 
 describe("AmountInputBase", () => {
   it("renders simple input when not connected (disabled)", () => {
