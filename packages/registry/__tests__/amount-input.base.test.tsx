1cfe4362609bd20bc740b34dea1f4b57ea54bacd
Niklas P <contact@niklasplessing.net>
2025-09-29 10:45:31 +0200

    test

--- packages/registry/__tests__/amount-input.base.test.tsx
+++ packages/registry/__tests__/amount-input.base.test.tsx
@@ -5,10 +5,9 @@ import "@testing-library/jest-dom";
 import { AmountInputBase } from "@/registry/polkadot-ui/blocks/amount-input/components/amount-input.base";
 
 describe("AmountInputBase", () => {
-  it("renders simple input when not connected (disabled)", () => {
+  it("renders simple input enabled by default", () => {
     render(<AmountInputBase value="" />);
     const input = screen.getByRole("spinbutton");
-    // Component does not render a visible label; label prop is forwarded as an attribute
-    expect(input).toBeDisabled();
+    expect(input).toBeEnabled();
   });
 });
