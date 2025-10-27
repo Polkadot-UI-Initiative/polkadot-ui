c0124c237a0016e13cc1eaa8c7ff354bb3378f8c
Niklas P <contact@niklasplessing.net>
2025-10-27 12:11:58 +0100

    add llms full

--- packages/registry/source.config.ts
+++ packages/registry/source.config.ts
@@ -1,5 +1,10 @@
 import { defineDocs } from "fumadocs-mdx/config";
 
 export const docs = defineDocs({
+  docs: {
+    postprocess: {
+      includeProcessedMarkdown: true,
+    },
+  },
   dir: "app/docs",
 });
